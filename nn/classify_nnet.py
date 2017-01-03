8b4e67573b5b02ba7592f4095bb00aebd115305d
Skanda Koppula <skoppula@mit.edu>
2017-01-03 16:48:52 -0500

    checkpoint

--- nn/classify_nnet.py
+++ nn/classify_nnet.py
@@ -2,152 +2,110 @@
 
 import numpy as np
 import os
-import gc
 
 # all utterance data [utt1 mfcc frames [N1 x 60], utt2 frames]
 def mkdir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def get_verify_lbls(y, curr_spk):
-    return np.array(list(map(lambda x: 1 if x == curr_spk else 0, y)))
-
-def conv_to_ver_and_one_hot_encode(y, curr_spk):
-    # convert to verification task
-    ver_lbls = get_verify_lbls(y, curr_spk)
+# assumes inputs in y from 0 ... n
+def one_hot_encode(y, num_spks):
     num_frames = np.shape(y)[0]
-
-    one_hot_lbls = np.zeros((num_frames, 2))
-    one_hot_lbls[np.arange(num_frames), ver_lbls] = 1
+    one_hot_lbls = np.zeros((num_frames, num_spks))
+    one_hot_lbls[np.arange(num_frames), y] = 1
     return one_hot_lbls
 
-def get_ver_network_arch(model_path, n_inp_frms):
+def get_class_net(model_path, n_inp_frms, num_spks):
 
     from keras.models import Sequential
     from keras.layers.core import Dense, Dropout, Activation
+    from keras.layers.advanced_activations import LeakyReLU
     from keras.utils.visualize_util import plot
-    from keras.optimizers import Adam
+    from keras.optimizers import Nadam
 
     # Add batch normalization: keras.layers.normalization.BatchNormalization()
     model = Sequential()
-    model.add(Dense(128, input_shape=(n_inp_frms*60,))) # 60 MFCCs / frame
-    model.add(Activation('relu'))
+    model.add(Dense(64, input_shape=(n_inp_frms*60,), activation='linear')) # 60 MFCCs / frame
+    model.add(LeakyReLU())
     model.add(Dropout(0.2))
-    model.add(Dense(64))
-    model.add(Activation('relu'))
+    model.add(Dense(64, activation='linear'))
+    model.add(LeakyReLU())
     model.add(Dropout(0.2))
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dense(2))
+    model.add(Dense(num_spks))
     model.add(Activation('softmax'))
 
     model.summary()
     plot(model, to_file=model_path + 'architecture.png')
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(),
-                  metrics=['accuracy'])
+                  optimizer=Nadam(),
+                  metrics=['accuracy', 'precision', 'recall'])
     return model
 
-def evaluate_activations(model, X, layer):
-    from keras.layers.core import K
-    get_layer_output = K.function([model.layers[0].input, K.learning_phase()], [model.layers[layer].output])
-    return get_layer_output([X, 0])[0]
+def train_network(model, tr_x, tr_y, model_path, n_epochs=250, batch_size=50):
 
-def train_and_test_network(model, tr_x, tr_y, te_x, te_y, curr_spk, model_path, n_epochs=75, batch_size=50):
+    from keras.callbacks import ModelCheckpoint
 
-    from keras.optimizers import Adam
-    from keras.callbacks import ModelCheckpoint, EarlyStopping
-
-    saved_model_path = model_path + "curr_best_weights_"  + str(curr_spk) + ".hdf5"
+    saved_model_path = model_path + "curr_best_weights.hdf5"
     ckpt = ModelCheckpoint(saved_model_path, monitor='val_acc', verbose=0, save_best_only=False, mode='max')
-    early_stop = EarlyStopping(monitor='val_loss', patience=0, verbose=1, mode='auto')
-    trn_history = model.fit(tr_x, tr_y, validation_split=0.2,
+    # early_stop = EarlyStopping(monitor='val_loss', patience=0, verbose=1, mode='auto')
+    trn_history = model.fit(tr_x, tr_y, validation_split=0.1, shuffle=True,
                         batch_size=batch_size, nb_epoch=n_epochs, verbose=1,
-                        callbacks=[ckpt, early_stop])
+                        callbacks=[ckpt])
 
-    np.save(model_path + "history_" + str(curr_spk) + ".npy", trn_history.history)
+    np.save(model_path + "history.npy", trn_history.history)
 
-    model.load_weights(saved_model_path)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(),
-                  metrics=['accuracy'])
+def remap_spk_ids(enr_y, ver_y):
+    # REMAP SPEAKER IDs TO 0..n
+    spk_mappings = {}
+    curr_map = 0
+    all_spks = set(enr_y).union(set(ver_y))
+    for spk in all_spks:
+        if spk not in spk_mappings:
+            spk_mappings[spk] = curr_map
+            curr_map += 1
+    map_spks = np.vectorize(lambda x: spk_mappings[x])
+    print("Speaker Re-mappings:", spk_mappings)
+    return all_spks, map_spks(enr_y), map_spks(ver_y), spk_mappings
 
-    # del tr_x, tr_y, val_x, val_y # for saving memory
-    score = model.evaluate(te_x, te_y, verbose=0)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
+def read_data(path, n_inp_frms):
+    x = np.load(path + "X.npy")
+    n_frames = np.shape(x)[0]
+    x = x.reshape(n_frames, N_INP_FRMS * 60) # 60 MFCCs per frame
+    y = np.load(path + "y.npy")
+    assert n_frames == np.shape(y)[0]
+    return x, y
 
-    gc.collect() # fix suggested by https://github.com/tensorflow/tensorflow/issues/3388
+def get_params():
+    N_INP_FRMS = 120
+    BASE_PATH = "/home/skoppula/biometrics/data/yoho/kaldi_yoho/data/"
+    MODEL_PATH = 'classify_model_' + str(N_INP_FRMS) + '/'
+    mkdir(MODEL_PATH)
+    VER_PATH = BASE_PATH + "verify/final/nn_inp-" + str(N_INP_FRMS) + "_frames/"
+    ENR_PATH = BASE_PATH + "enroll/final/nn_inp-" + str(N_INP_FRMS) + "_frames/"
 
-    model.load_weights(saved_model_path)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(),
-                  metrics=['accuracy'])
-
-    activations = evaluate_activations(model, te_x, 9)
-    np.save(model_path + "activations_" + str(curr_spk) + ".npy", activations)
-
-    predictions = model.predict(te_x, batch_size=batch_size, verbose=0)
-    np.save(model_path + "predictions_" + str(curr_spk) + ".npy", predictions)
-
-    return score[1]
-
+    return N_INP_FRMS, BASE_PATH, MODEL_PATH, VER_PATH, ENR_PATH
 
 if __name__ == "__main__":
     np.random.seed(1337) # reproducibility
 
-    for N_INP_FRMS in [60, 80, 120]:
-        print("\nNUM INPUT FRAMES:",N_INP_FRMS,"\n")
-        MODEL_PATH = 'tsne_model_' + str(N_INP_FRMS) + '/'
-        mkdir(MODEL_PATH)
-        BASE_PATH = "/home/skoppula/biometrics/data/yoho/kaldi_yoho/data/"
-        VER_PATH = BASE_PATH + "verify/final/nn_inp-" + str(N_INP_FRMS) + "_frames/"
-        ENR_PATH = BASE_PATH + "enroll/final/nn_inp-" + str(N_INP_FRMS) + "_frames/"
+    params = get_params()
+    N_INP_FRMS, BASE_PATH, MODEL_PATH, VER_PATH, ENR_PATH = params
+    print("PARAMS: ", params)
 
-        print("Model(s) Path: ", MODEL_PATH)
-        print("Using verification path: ", VER_PATH)
-        print("Using enroll data path: ", ENR_PATH)
+    enr_x, enr_y = read_data(ENR_PATH, N_INP_FRMS)
+    _, ver_y = read_data(VER_PATH, N_INP_FRMS)
 
-        # ENROLL DATA LOAD
-        enr_x = np.load(ENR_PATH + "X.npy")
-        n_frames = np.shape(enr_x)[0]
-        enr_x = enr_x.reshape(n_frames, N_INP_FRMS * 60) # 60 MFCCs per frame
-        enr_y = np.load(ENR_PATH + "y.npy")
-        assert n_frames == np.shape(enr_y)[0]
-        print("Enroll X shape", np.shape(enr_x))
-        print("Enroll y shape", np.shape(enr_y))
+    print("Enroll X shape", np.shape(enr_x))
+    print("Enroll y shape", np.shape(enr_y))
+    print("Verify y shape", np.shape(ver_y))
 
-        # VERIFY DATA LOAD
-        ver_x = np.load(VER_PATH + "X.npy")
-        n_frames = np.shape(ver_x)[0]
-        ver_x = ver_x.reshape(n_frames, N_INP_FRMS * 60)
-        ver_y = np.load(VER_PATH + "y.npy")
-        assert n_frames == np.shape(ver_y)[0]
-        print("Verify X shape", np.shape(ver_x))
-        print("Verify y shape", np.shape(ver_y))
+    all_spks, enr_y, _, _ = remap_spk_ids(enr_y, ver_y)
+    model = get_class_net(MODEL_PATH, N_INP_FRMS, len(all_spks))
 
-        poss_spks = list(set(np.load(VER_PATH + "y.npy")))
+    enr_y = one_hot_encode(enr_y, len(all_spks))
+    print("One-Hot Enroll (y) shape", np.shape(enr_y))
 
-        test_accs = []
-        for i, curr_spk in enumerate(poss_spks):
-            print("EVALUATING AND TRAINING FOR CURRENT SPEAKER:", curr_spk, str(i) + "/" + str(len(poss_spks)))
-            model = get_ver_network_arch(MODEL_PATH, N_INP_FRMS)
-
-            # oversampling data augmentation to counteract imbalanced dataset
-            OVERSAMPLING_FACTOR = 60
-            pos_idxs = np.where(enr_y == curr_spk)
-            enr_y = np.concatenate((np.repeat(enr_y[pos_idxs], OVERSAMPLING_FACTOR), enr_y))
-            enr_x = np.concatenate((np.repeat(enr_x[pos_idxs], OVERSAMPLING_FACTOR, axis=0), enr_x), axis=0)
-
-            tr_y = conv_to_ver_and_one_hot_encode(enr_y, curr_spk)
-            te_y = conv_to_ver_and_one_hot_encode(ver_y, curr_spk)
-            spk_path = MODEL_PATH + str(curr_spk) + "/"
-            mkdir(spk_path)
-            test_acc = train_and_test_network(model, enr_x, tr_y, ver_x, te_y, curr_spk, spk_path)
-            test_accs.append(test_acc)
-
-        gc.collect()
-        print("TEST ACCURACIES:",test_accs)
-        print("Final average test accuracy:", sum(test_accs)/len(test_accs))
+    train_network(model, enr_x, enr_y, MODEL_PATH)
+    print("Training done!")
 
--- nn/tsne.py
+++ nn/tsne.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[44]:
+# In[3]:
 
 from sklearn.manifold import TSNE
 import numpy as np
@@ -10,91 +10,88 @@ import verify_nnet
 get_ipython().magic('matplotlib inline')
 
 
-# In[63]:
+# In[ ]:
 
 model = TSNE(n_components=2, perplexity=50, random_state=1)
 spk = "101"
 
 
-# In[68]:
-
-activations = np.load("tsne_model_60/" + spk + "/activations_" + spk + ".npy")
-activations.shape
-
-
 # In[ ]:
-
-
-
-
-# In[95]:
-
-BASE_PATH = "/home/skoppula/biometrics/data/yoho/kaldi_yoho/data/"
-VER_PATH = BASE_PATH + "verify/final/nn_inp-60_frames/"
-lbls = np.load(VER_PATH + "y.npy")
-te_x = np.load(VER_PATH + "X.npy")
-te_y = verify_nnet.get_verify_lbls(lbls, int(spk))
-te_y.shape
-
-
-# In[97]:
-
-pidxs = np.where(te_y[0:300]==1)[0]
-
-
-# In[108]:
-
-te_x.shape
-
-
-# In[110]:
-
-np.concatenate((np.repeat(lbls[pidxs], 2), lbls))
-np.concatenate((np.repeat(te_x[pidxs], 10, axis=0), te_x), axis=0).shape
-
-
-# In[112]:
-
-
-
-
-# In[89]:
-
-y = np.bincount(lbls)
-ii = np.nonzero(y)[0]
-list(zip(ii,y[ii])), len(lbls)
-
-
-# In[86]:
-
-preds = np.load("tsne_model_60/" + spk + "/predictions_" + spk + ".npy")
-preds.shape
-diffs = np.where(preds[:,0] < preds[:,1])
-
-
-# In[87]:
-
-pl_act = activations[0:300]
-pl_te = te_y[0:300]
-pl_pred = preds[0:300]
-# print(sum(te_y))
-# print(te_y[104:112])
-print(lbls[104:112])
-print(pl_act[104:112])
-print(pl_pred[104:112])
-# print(pl_act.shape, pl_te.shape)
-preds[diffs], activations[diffs]
-
-
-# In[53]:
 
 out = model.fit_transform(pl_act)
 
 
-# In[54]:
+# In[ ]:
 
 plt.title("BH-SNE Embedding of Last Layer ReLU activations")
 plt.scatter(out[:,0], out[:,1], c=pl_te, cmap=plt.cm.get_cmap("prism", 2))
 plt.colorbar(ticks=range(1,3))
 plt.show()
+
+
+# In[4]:
+
+N_INP_FRMS = 120
+
+print("\nNUM INPUT FRAMES:",N_INP_FRMS,"\n")
+BASE_PATH = "/home/skoppula/biometrics/data/yoho/kaldi_yoho/data/"
+VER_PATH = BASE_PATH + "verify/final/nn_inp-" + str(N_INP_FRMS) + "_frames/"
+ENR_PATH = BASE_PATH + "enroll/final/nn_inp-" + str(N_INP_FRMS) + "_frames/"
+
+print("Model(s) Path: ", MODEL_PATH)
+print("Using verification path: ", VER_PATH)
+print("Using enroll data path: ", ENR_PATH)
+
+# ENROLL DATA LOAD
+enr_x = np.load(ENR_PATH + "X.npy")
+n_frames = np.shape(enr_x)[0]
+enr_x = enr_x.reshape(n_frames, N_INP_FRMS * 60) # 60 MFCCs per frame
+enr_y = np.load(ENR_PATH + "y.npy")
+assert n_frames == np.shape(enr_y)[0]
+print("Enroll X shape", np.shape(enr_x))
+print("Enroll y shape", np.shape(enr_y))
+
+# VERIFY DATA LOAD
+ver_x = np.load(VER_PATH + "X.npy")
+n_frames = np.shape(ver_x)[0]
+ver_x = ver_x.reshape(n_frames, N_INP_FRMS * 60)
+ver_y = np.load(VER_PATH + "y.npy")
+assert n_frames == np.shape(ver_y)[0]
+print("Verify X shape", np.shape(ver_x))
+print("Verify y shape", np.shape(ver_y))
+
+
+# In[15]:
+
+enr_spks = set(enr_y)
+ver_spks = set(ver_y)
+
+print(len(enr_spks), len(ver_spks))
+print(min(enr_spks), max(enr_spks), max(enr_spks)-min(enr_spks)+1)
+print(enr_spks - ver_spks)
+print(ver_spks - enr_spks)
+
+
+# In[16]:
+
+spk_mappings = {}
+curr_map = 0
+for spk in enr_spks.union(ver_spks):
+    if spk not in spk_mappings:
+        spk_mappings[spk] = curr_map
+        curr_map += 1
+map_spks = np.vectorize(lambda x: spk_mappings[x])
+enr_y = map_spks(enr_y)
+ver_y = map_spks(ver_y)
+
+
+# In[20]:
+
+enr_spks = set(enr_y)
+ver_spks = set(ver_y)
+
+print(len(enr_spks), len(ver_spks))
+print(min(enr_spks), max(enr_spks), max(enr_spks)-min(enr_spks)+1)
+print(enr_spks - ver_spks)
+print(ver_spks - enr_spks)
 
